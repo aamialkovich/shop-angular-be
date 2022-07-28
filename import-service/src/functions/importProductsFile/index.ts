ffed2e9b9e9e41dd07ce60008181c6f8931821d6
Aliaksandr Amialkovich <aamialkovich@infoblox.com>
2022-07-28 14:57:23 +0300

    feat: import service init

---
+++ import-service/src/functions/importProductsFile/index.ts
@@ -0,0 +1,25 @@
+import schema from './schema';
+import { handlerPath } from '@libs/handler-resolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      http: {
+        method: 'get',
+        path: '/import',
+        cors: true,
+        request: {
+          schemas: {
+            'application/json': schema,
+          },
+          parameters: {
+            querystrings: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  ],
+};
--- product-service/utils/responseMessages.js
+++ product-service/utils/responseMessages.js
@@ -2,5 +2,5 @@ export const responseMessages = {
   NOT_FOUND: 'Product was not found.',
   SERVER_ERROR: 'Internal Error Server.',
   INVALID_DATA: 'Some data was invalid. Please, try again.',
-  SUCCESSFULLY_CREATED: 'Product was successfully created.'
+  SUCCESSFULLY_CREATED: 'Product was successfully created.',
 };
